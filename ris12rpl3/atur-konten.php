b1ce73bf8acc84c7e788ff1882e5ffba4ab52ac2
rstna269 <93703270+rstna269@users.noreply.github.com>
2022-01-27 11:02:18 +0700

    Delete ris12rpl3 directory

--- ris12rpl3/atur-konten.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ($_GET['menu']= 'registrasi'){
-    print "<h1>FORM REGISTRASI......</h1>";
-}
--- ris12rpl3/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-//belum jalan/hancaa
-$konek = mysqli_connect("127.0.0.1", "root", "passwd48", "ukk_2020_p2");
-
-if ($konek) {
-    print "terhubung";
-} else {
-    print "tidak terhubung...";
-}
